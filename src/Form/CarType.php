6e944611a319cec9b7d4ea5b877144286d506eac
yassine ben ghanem <benghanem1999@gmail.com>
2021-06-01 19:33:01 +0100

    crud Car and Driver

---
+++ src/Form/CarType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('model')
+            ->add('registration_number')
+            ->add('number_places')
+            ->add('driver_id')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Car::class,
+        ]);
+    }
+}
---
+++ src/Form/DriverType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Driver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DriverType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('first_mame')
+            ->add('last_name')
+            ->add('number_tel')
+            ->add('account_email')
+            ->add('car')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Driver::class,
+        ]);
+    }
+}
